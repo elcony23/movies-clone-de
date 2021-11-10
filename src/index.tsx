f25c8d1141859b1b581e9cc2a261b875e379ea26
Donaldo Avila <donaldo@livement.com>
2021-11-10 07:41:18 -0700

    Redux persist added

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
 import { store } from './store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
         <App />
+        </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store.js
+++ src/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers,getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+  } from 'redux-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage';
+import autoMergeLevel1 from 'reduxjs-toolkit-persist/lib/stateReconciler/autoMergeLevel1'
 import moviesReducer from './reducers/movies';
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel1,
+};
+const reducers = combineReducers({
+    movies: moviesReducer
+});
+const _persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    movies: moviesReducer,
-  },
-});
+    reducer: _persistedReducer,
+    middleware: getDefaultMiddleware({
+      serializableCheck: {
+        /* ignore persistance actions */
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER
+        ],
+      },
+    }),
+  });
